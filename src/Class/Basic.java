5b40478a9dac0e488964b17dc1e9c05558857531
ApacheT101 <weixian1999@gmail.com>
2019-12-12 07:52:44 +0800

    Add files via upload

---
+++ src/Class/Basic.java
@@ -0,0 +1,6 @@
+package Class;
+
+public abstract class Basic {
+    // make id and name save as a string, make it abstract because this is the most important component in our app
+    protected String id = " ", name = " ";
+}
